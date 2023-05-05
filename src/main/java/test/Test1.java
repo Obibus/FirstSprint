f48398e91640a60b392a5d69b64fabe97e957a5a
Maks Bidziura <maksbidzura@gmail.com>
2023-05-05 11:27:42 +0300

    testcommit

---
+++ src/main/java/test/Test1.java
@@ -0,0 +1,15 @@
+package test;
+import org.junit.Test;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+public class Test1 {
+
+}
